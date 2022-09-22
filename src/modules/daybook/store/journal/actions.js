a39e0cc7281c407229605061c4f0c8b6f62ef163
Arabath <axelhobrecht@hotmail.com>
2022-09-22 09:37:20 -0300

    Cargar entradas desde el Backend

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -1,5 +1,9 @@
+import journalApi from "@/api/journalApi"
+
 export const loadEntries = async () => {
 
+    const { data } = await journalApi.get('/entries.json')
+    console.log(data)
 }
 
 export const updateEntry = async () => {
